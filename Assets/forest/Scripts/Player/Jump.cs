1c96c43467edea80f7ca7803eb0680a026d035db
DarioMirandaYT <66534526+vicotux1@users.noreply.github.com>
2023-03-28 20:38:53 -0300

    002

--- Assets/forest/Scripts/Player/Jump.cs
+++ Assets/forest/Scripts/Player/Jump.cs
@@ -17,9 +17,7 @@ public class Jump : MonoBehaviour{
     }
     void Update() {
         Jumping();
-        if (controller.isGrounded) {
-            Debug.Log("puedes saltar");
-            } 
+     
      }   
         
      void Jumping() {
