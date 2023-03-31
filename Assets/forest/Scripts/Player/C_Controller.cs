60391272c9dc80ceada62619d425ac5e1b203426
DarioMirandaYT <66534526+vicotux1@users.noreply.github.com>
2023-03-31 18:17:00 -0300

    0.56.2

--- Assets/forest/Scripts/Player/C_Controller.cs
+++ Assets/forest/Scripts/Player/C_Controller.cs
@@ -7,30 +7,48 @@ public class C_Controller : MonoBehaviour{
    [SerializeField]bool CursorIsvisble=false;
     [SerializeField]string Horizontal, Vertical;
     public float Speed;
-    
-    
+    private  CharacterController _controller;
+    Vector3 _move;
     float Axis_Horizontal, Axis_Vertical;
     Animator _anim;
     private void Start() {
         _anim=gameObject.GetComponent<Animator>();
+        _controller=GetComponent<CharacterController>();
     }
     void Update(){
         Axis_Horizontal=Input.GetAxis(Horizontal);
         Axis_Vertical=Input.GetAxis(Vertical);
-        Movement(Axis_Horizontal,Axis_Vertical);
+        _move=new Vector3(Axis_Vertical,0,Axis_Horizontal);
         Cursor.visible = CursorIsvisble;
-        
+        Rotate(_move);
+        Movement(_move);       
     } 
 
-        void Movement(float vertical, float horizontal){ 
-        Vector3 Move=new Vector3(horizontal,0,vertical);
-        Move.Normalize();
-        transform.Translate(Move * Time.deltaTime * Speed);       
-          if (Axis_Horizontal!=0 || Axis_Vertical!=0){           
+        void Movement(Vector3 Moverse){ 
+       
+    // _move.Normalize();
+        _controller.Move(Moverse * Time.deltaTime * Speed);       
+          if (Axis_Horizontal!=0){           
                  _anim.SetBool("IsRunning",true);             
-        }else{           
-                 _anim.SetBool("IsRunning",false);             
+        }if (Axis_Horizontal==0){                 
+             _anim.SetBool("IsRunning",false);             
+        }if (Axis_Vertical>=0.1f){                 
+             _anim.SetFloat("Speed",1.0f);             
+        }if (Axis_Vertical==0){                 
+             _anim.SetFloat("Speed",0.0f);             
+        }if (Axis_Vertical<=-1f){                 
+             _anim.SetFloat("Speed",1.0f);             
         }
-              
+        
+    
     } 
+      
+
+void Rotate(Vector3 move){
+        Vector3 Rotation=new Vector3 (move.x,0, move.z);        
+         if (Axis_Horizontal!=0 || Axis_Vertical!=0){
+          transform.rotation= Quaternion.LookRotation(Rotation);
+        }
+    }
+    
 }
