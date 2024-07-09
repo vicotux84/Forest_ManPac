912a122e1a9e0e50f68405e8e2050d6d459799f0
vicotux84 <victormirandamor84@gmail.com>
2024-07-09 00:03:08 -0300

    ManPac2_(0.5.8.02)

---
+++ Assets/MainMenu/scripts/SmoothFollow.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+	public class SmoothFollow : MonoBehaviour{
+
+		
+		[Range(0.0f, 10.0f)]public float distance = 10.0f;
+		[Range(0.0f, 10.0f)]public float height = 5.0f;
+		[Range(0.0f, 10.0f)]public float rotationDamping;
+		[Range(0.0f, 10.0f)]public float heightDamping;
+		public string TagFollowTarget="Player";
+
+		private Transform target;		
+
+		void Update (){
+		Buscar ();
+	}
+
+	void Buscar(){
+		if(target==null){
+		target = GameObject.FindWithTag (TagFollowTarget).transform;
+		} 
+	}
+			void LateUpdate(){
+			if (!target)
+				return;
+			var wantedRotationAngle = target.eulerAngles.y;
+			var wantedHeight = target.position.y + height;
+			var currentRotationAngle = transform.eulerAngles.y;
+			var currentHeight = transform.position.y;
+			currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+			currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+			var currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+			transform.position = target.position;
+			transform.position -= currentRotation * Vector3.forward * distance;
+			transform.position = new Vector3(transform.position.x ,currentHeight , transform.position.z);
+			transform.LookAt(target);
+	}
+
+}
